efd3c541577a939f579351deec97d21ab1d4758e
christopher-rice <christopher.rice24444@gmail.com>
2023-09-01 14:10:56 -0500

    Added includes for glm library

--- 4_transformations/main.cpp
+++ 4_transformations/main.cpp
@@ -3,6 +3,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "shader.h"
 #include "stb_image.h"
 
